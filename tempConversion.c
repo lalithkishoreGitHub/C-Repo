f512105d8f66ab1180c5878790896591fb153c5c
lalithkishoreGitHub <lalithkishore2401@gmail.com>
2023-01-03 06:47:17 +0530

    C Program File

---
+++ tempConversion.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main() {
+
+float f,c;
+
+printf("Enter Celsius temperature\n");
+scanf("%f",&c);
+//Conversion
+f=(float)((9*c)/5)+32;//Farenheit to Celsius
+printf("Farenheit temperature: %f\n",f);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
