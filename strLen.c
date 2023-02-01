a2f90f7fb93c15a4b52a4c4961c983119bbc60d8
lalithKishoreGitHub <lalithkishore2401@gmail.com>
2023-02-01 06:59:13 +0530

    C Practical programs

---
+++ strLen.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main() {
+    char str[20];
+    int i;
+    printf("Enter a string:  ");
+    scanf("%s",str);
+    while(str[i]!='\0') {
+       ++i;
+}
+     printf("Length of the string is %d \n",i);
+}
