20225ab710da56e3ddfb9c2f7247870439021007
lalithkishoreGitHub <119030108+lalithkishoreGitHub@users.noreply.github.com>
2023-02-21 06:39:50 +0530

    Update strCmp.c

--- C-example files/strCmp.c
+++ C-example files/strCmp.c
@@ -1,15 +1,27 @@
-#include<stdio.h>
+int stringLength(char str[]) {
+    int length=0;
+    while(str[length]!='\0'){
+        length++;
+    }
+    return length;
+}
 
 int stringCompare(char *s, char *d) {
     int i;
-    for(i=0;s[i]==d[i];i++){
-        if(s[i]!='\0') {
-            return s[i]-d[i];
-        }else{
+    if(stringLength(s)==stringLength(d)) {
+    for(i=0;s[i]!='\0'&&d[i]!='\0';i++){
+        if(s[i]==d[i]) {
             return 0;
+        }else{
+            return 1;
         }
     }
-} 
+ }
+ else {
+    return 1;
+ }
+}
+ 
 int main() {
     char strA[20], strB[20];
     printf("Enter first string: ");
@@ -17,9 +29,9 @@ int main() {
     printf("Enter second string: ");
     scanf("%s",strB);
     if(stringCompare(strA,strB)==0) {
-        printf("%s is smaller than %s\n",strA,strB);
-    }else if(stringCompare(strA,strB)<0){
-        printf("%s is bigger than %s\n",strA,strB);
+        printf("%s is the same as %s\n",strA,strB);
+    }else if(stringCompare(strA,strB)!=0){
+        printf("%s is not the same as %s\n",strA,strB);
     }
 
 }
