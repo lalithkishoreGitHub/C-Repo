93c40f763bd69e2098a64f6b161afeb19344daa9
lalithkishoreGitHub <119030108+lalithkishoreGitHub@users.noreply.github.com>
2023-02-22 00:02:54 +0530

    Update cpyArr.c

--- C-example files/cpyArr.c
+++ C-example files/cpyArr.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
 //swapping of arrays using pointers
 void main() {
-    int a[10],b[10],i,*atr=&a[0],*btr=&b[0];
+    int a[10],b[10],i;
     printf("Enter array elements one by one: ");
     for(i=0;i<5;i++){
         scanf("%d",&b[i]);
     }
     for(i=0;i<5;i++){
         *(a+i)=*(b+i);
-        atr++;
-        btr++;
     }
     printf("After copying: \n");
     for(i=0;i<5;i++){
         printf("%d ",*(a+i));
-        atr++;
-        if(atr==&a[4]){
-            break;
-        }
     }
     printf("\n");
 }
