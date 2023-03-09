4b2901de3c3ade52ca9bd0ce32bcb6951964df01
lalithkishoreGitHub <119030108+lalithkishoreGitHub@users.noreply.github.com>
2023-03-09 19:38:42 +0530

    Update matProdPtr.c

--- C-example files/matProdPtr.c
+++ C-example files/matProdPtr.c
@@ -1,6 +1,4 @@
-#include<stdio.h>
-
-int a[10][10],b[10][10],p[10][10],i,j,k,m1,n1,m2,n2;
+int a[10][10],b[10][10],p[10][10],i,j,k,m1,n1,m2,n2,*c[10][10],*d[10][10],*q[10][10];//array of pointers
 void input() {
     printf("Enter the order of first matrix A: ");
     scanf("%d%d",&m1,&n1);
@@ -13,19 +11,22 @@ void computeProduct() {
         for(i=0;i<m1;i++){
             for(j=0;j<n1;j++){
                 scanf("%d",&a[i][j]);
+                c[i][j]=&a[i][j];
             }
         }
         printf("Enter the elemnets of second matrix B\n");
         for(i=0;i<m2;i++){
             for(j=0;j<n2;j++){
                 scanf("%d",&b[i][j]);
+                d[i][j]=&b[i][j];
             }
         }
         for(i=0;i<m1;i++){
             for(j=0;j<n2;j++){
-                *(*(p+i)+j)=0;
+                q[i][j]=&p[i][j];
+                *q[i][j]=0;
                 for(k=0;k<n1;k++){
-                    *(*(p+i)+j)= *(*(p+i)+j) + (*(*(a+i)+k)) * (*(*(b+k)+j));
+                   *q[i][j]=*q[i][j]+((*c[i][k])*(*d[k][j]));
                 }
             }
         }
@@ -37,14 +38,9 @@ void output() {
     printf("Product:\n");
     for(i=0;i<m1;i++){
             for(j=0;j<n2;j++){
-               printf("%d  ",*(*(p+i)+j));
+               printf("%d  ",*q[i][j]);
             }
             printf("\n");
         }
             printf("\n");
-}
-void main() {
-    input();
-    computeProduct();
-    output();
 }
